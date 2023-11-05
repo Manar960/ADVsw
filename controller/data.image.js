6894ff8c01aafc357fa39442fd5b2af798d13412
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2023-11-05 12:25:30 +0200

    delete image
    delete image from Firebase

--- controller/data.image.js
+++ controller/data.image.js
@@ -1,14 +1,35 @@
 const { ref, uploadBytes } = require("firebase/storage");
 const storage = require("../config/fiarbase");
 const multer = require("multer");
+const {deleteObject } = require("firebase/storage");
 
-// تعريف أسماء الفايل المسموح بها
 const allowedFileNames = ['AirQuality', 'Humidity', 'WaterQuality', 'BiodiversityMetrics', 'WindSpeed', 'RainFall', 'PollutionLevels', 'SoilQuality', 'UvIndex', 'NoiseLevels', 'Weather'];
 
 const upload = multer({ storage: multer.memoryStorage() });
 
+
+exports.deleteImage =  (req, res) => {
+  const fileName = req.params.fileName;
+  const imageName = req.params.imageName;
+
+  if (!allowedFileNames.includes(fileName)) {
+    return res.status(400).json({ message: "Invalid file name" });
+  }
+
+  const storageRef = ref(storage, `files/${fileName}/${imageName}`);
+
+  try {
+    deleteObject(storageRef);
+    res.json({ message: "File deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting file:", error);
+    res.status(500).json({ message: "Failed to delete file" });
+  }
+};
+
+
 exports.uploadFile = (req, res, next) => {
-  const fileName = req.params.fileName; // اسم الفايل سيأتي من الطلب
+  const fileName = req.params.fileName;      
 
   if (!allowedFileNames.includes(fileName)) {
     return res.status(400).json({ message: "Invalid file name" });
--- routes/image.router.js
+++ routes/image.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { uploadFile } = require("../controller/data.image");
+const { deleteImage } = require("../controller/data.image");
+
 
 const firebase = require("firebase/app");
 
@@ -8,7 +10,8 @@ router.get("/", (req, res) => {
   res.json("Firebase Storage");
 });
 
-// استخدام عنوان متغير يسمى "fileName"
+
 router.post("/:fileName", uploadFile);
+router.delete("/:fileName/:imageName", deleteImage);
 
 module.exports = router;
