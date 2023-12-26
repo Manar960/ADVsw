5835cbc78a1ce151204b9a8455d904d13ac83f05
Aida Haqash <41109025+aidahaqash@users.noreply.github.com>
2023-12-26 18:40:54 +0200

    Score
    sustainability score done

--- controller/environment.data.controller.js
+++ controller/environment.data.controller.js
@@ -28,39 +28,39 @@ exports.createEnvironment = (req, res) => {
   }
 
   const sql = `
-    UPDATE environment
-    SET UserID = ?, AirQuality = ?, Humidity = ?, WaterQuality = ?,
-    BiodiversityMetrics = ?, WindSpeed = ?, RainFall = ?,
-    PollutionLevels = ?, SoilQuality = ?, UvIndex = ?,
-    NoiseLevels = ?, Weather = ?, EventDescription = ?, Note = ?
-    `;
-    const values = [
-      userID,
-      AirQuality,
-      Humidity,
-      WaterQuality,
-      BiodiversityMetrics,
-      WindSpeed,
-      RainFall,
-      PollutionLevels,
-      SoilQuality,
-      UvIndex,
-      NoiseLevels,
-      Weather,
-      EventDescription,
-      Note
-    ];
+    INSERT INTO environment (UserID, AirQuality, Humidity, WaterQuality,
+    BiodiversityMetrics, WindSpeed, RainFall, PollutionLevels, SoilQuality,
+    UvIndex, NoiseLevels, Weather, EventDescription, Note)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+  const values = [
+    userID,
+    AirQuality,
+    Humidity,
+    WaterQuality,
+    BiodiversityMetrics,
+    WindSpeed,
+    RainFall,
+    PollutionLevels,
+    SoilQuality,
+    UvIndex,
+    NoiseLevels,
+    Weather,
+    EventDescription,
+    Note
+  ];
 
-    db.query(sql, values, (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
-      } else {
-        res.status(201).json({ message: 'Environment record created' });
-      }
-    });
-  };
+  db.query(sql, values, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      updateScore(userID);
 
+      res.status(201).json({ message: 'Environment record created' });
+    }
+  });
+};
 
 // Retrieve 
 exports.getEnvironment = (req, res) => {
@@ -154,4 +154,88 @@ exports.getEnvironment = (req, res) => {
     });
   };
 
+  exports.updateScore = (userID) => {
+    if (!userID) {
+      return Promise.reject({ error: 'Missing required parameters' });
+    }
   
+    const getUserQuery = 'SELECT * FROM user WHERE UserID = ?';
+    return new Promise((resolve, reject) => {
+      db.query(getUserQuery, [userID], (err, userResult) => {
+        if (err) {
+          console.error(err);
+          reject({ error: 'Internal server error' });
+        }
+  
+        if (userResult.length === 0) {
+          reject({ message: 'User not found' });
+        }
+
+        const currentScore = userResult[0].score || 0;
+        const newScore = currentScore + 1;
+  
+        const updateScoreQuery = 'UPDATE user SET score = ? WHERE UserID = ?';
+        db.query(updateScoreQuery, [newScore, userID], (err, updateResult) => {
+          if (err) {
+            console.error(err);
+            reject({ error: 'Internal server error' });
+          }
+  
+          if (updateResult.affectedRows === 0) {
+            reject({ message: 'User not found' });
+          }
+  
+          resolve({ message: 'User score updated successfully' });
+        });
+      });
+    });
+  };
+  
+  
+  exports.updateReportFake = (req, res) => {
+    const dataID = req.params.dataId;
+  
+    if (!dataID) {
+      return res.status(400).json({ error: 'Missing required parameters' });
+    }
+  
+    const getEnvironmentQuery = 'SELECT * FROM environment WHERE DataID = ?';
+    db.query(getEnvironmentQuery, [dataID], (err, environmentResult) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+  
+      if (environmentResult.length === 0) {
+        return res.status(404).json({ message: 'Environment data not found' });
+      }
+  
+      const updateReportFakeQuery = 'UPDATE environment SET reportFake = reportFake - 1 WHERE DataID = ?';
+      db.query(updateReportFakeQuery, [dataID], (err, updateResult) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Internal server error' });
+        }
+  
+        if (updateResult.affectedRows === 0) {
+          return res.status(404).json({ message: 'Environment data not found' });
+        }
+
+        const userID = environmentResult[0].UserID; 
+        const decrementScoreQuery = 'UPDATE `user` SET `score` = `score` - 1 WHERE `UserID` = ?';
+        db.query(decrementScoreQuery, [userID], (err, decrementResult) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal server error' });
+          }
+  
+          if (decrementResult.affectedRows === 0) {
+            return res.status(404).json({ message: 'User not found' });
+          }
+  
+          return res.status(200).json({ message: 'Report fake status updated successfully' });
+        });
+      });
+    });
+};
+
